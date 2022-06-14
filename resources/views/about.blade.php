f8ff5613faa9cbc8ad0797fdffc2cbdb6ae61551
alexanderJr <alexmarcjr21@gmail.com>
2022-06-14 21:01:32 +0100

    cleaning...

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>About us!</h1>
+<a href="/">Voltar para home</a>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  $nome = 'Alex';
+  $idade = 23;
+    return view('welcome', ['nome' => $nome, 'idade' => $idade]);
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
